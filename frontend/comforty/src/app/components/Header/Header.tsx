770c66d6fa005e046d0dad1a8d65f692174de677
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2025-01-24 19:30:42 +0500

    Get Started frontend on Jan 24, 25

---
+++ frontend/comforty/src/app/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <section>
+        <div>
+          <p>✔ Free Shipping On All Orders Over $50</p>
+        </div>
+        <div>
+          <div>
+            <span>Eng</span>
+            <span>Fags</span>
+            <span>Need Help</span>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <div>logo</div>
+          <div>Comforty</div>
+        </div>
+        <div>
+          <div>Icon</div>
+          <div>Card</div>
+          <div>Icon</div>
+        </div>
+      </section>
+
+      <section>
+        <div></div>
+        <div></div>
+      </section>
+    </div>
+  )
+}
+
+export default Header
