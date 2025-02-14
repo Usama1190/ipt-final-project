a56e0cc57d2e8ef21a207f8488130704bc372c70
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2025-02-14 23:44:21 +0500

    Hero pages layouting complete on feb 14, 25

--- university-event-management-frontend/src/components/common/HeroPageCom/HeroPageCom.jsx
+++ university-event-management-frontend/src/components/common/HeroPageCom/HeroPageCom.jsx
@@ -5,17 +5,34 @@ const HeroPageCom = ({ imgTitle }) => {
   return (
     <div className={styles.hpcow}>
       <div className={styles.hpciw}>
-        <div>
+        <div className={styles.hpchlw}>
           <a href="/">Home</a>
-          <span>
+          <span className={styles.hpcicw}>
             <i className="fa-sharp fa-solid fa-play"></i>
           </span>
           <a href="/">Main page</a>
         </div>
-        <div>
+        <div className={styles.hpchiw}>
             <HeroPageImgCom imgTitle={imgTitle} />
         </div>
-        HeroPageCom
+
+        <div className={styles.hpccow}>
+          <div className={styles.hpcciw}>
+            <div className={styles.hpccidw}>
+              <div className={styles.hpccidow}></div>
+              <div className={styles.hpccidtw}></div>
+              <div className={styles.hpccidhw}></div>
+            </div>
+          </div>
+
+          <div className={styles.tw}>
+            <h3>Main Page</h3>
+          </div>
+          <div className={styles.do}>
+            <div className={styles.loc}></div>
+            <div className={styles.poc}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
